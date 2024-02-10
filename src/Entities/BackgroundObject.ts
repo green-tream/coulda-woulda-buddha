a6e9b2a5a90dd58511ee692b4f217befa63d6da7
Brace <11965313+DoctorBracewell@users.noreply.github.com>
2024-02-10 13:28:01 +0000

    commit

--- src/Entities/BackgroundObject.ts
+++ src/Entities/BackgroundObject.ts
@@ -1,14 +1,7 @@
 import Entity from "./Entity";
 
-
 export default class BackgroundObject extends Entity {
-
-
-
-    constructor(width: number, height: number, texture: string) {
-        super(width, height, texture);
-    }
-
-
-
+	constructor(width: number, height: number, texture: string) {
+		super(width, height, texture);
+	}
 }
--- src/scenes/MainMenuScene.ts
+++ src/scenes/MainMenuScene.ts
@@ -1,28 +1,15 @@
 import * as PIXI from "pixi.js";
 import { Scene } from "../engine/Scene";
 import { app } from "../index";
+import BackgroundObject from "../entities/BackgroundObject";
 
 export default class MainMenuScene extends Scene {
-	private header: PIXI.Text;
-	private background: PIXI.Graphics;
-
 	async init(assets) {
-		this.assets = assets;
-		this.background = new PIXI.Graphics();
+		console.log(assets);
+		new BackgroundObject(1920, 1080, "background");
 	}
 
-	async start() {
-		this.background.drawRect(0, 0, 800, 800);
+	async start() {}
 
-		this.container.addChild(this.background);
-
-		setTimeout(async () => {
-			await app.scenes.start("queens");
-		}, 5000);
-	}
-
-	async update(delta: number) {
-		this.background.beginFill(Math.random() * 1000 * 0x000001);
-		this.background.drawRect(0, 0, 800, 800);
-	}
+	async update(delta: number) {}
 }
