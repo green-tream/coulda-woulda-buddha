ffd4b529fd8025589fc51f00c87564d101741a6b
Oliver Greenwood <greenwood672.dev@gmail.com>
2024-02-11 01:16:35 +0000

    Commited to get new screen movement

--- src/scenes/MainMenuScene.ts
+++ src/scenes/MainMenuScene.ts
@@ -53,7 +53,7 @@ export default class MainMenuScene extends Scene {
 	}
 
 	async start() {
-		this.playButton.getSprite().on("click", () => fadeToScene(this, "kings"));
+		this.playButton.getSprite().on("click", () => fadeToScene(this, "queens"));
 
 		this.buddha.getSprite().on("click", () => {
 			if (this.buddhaOpen) return;
